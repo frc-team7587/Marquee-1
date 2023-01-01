b9602140d0ab3bcb367f6d5aeb3dff25de06c823
Eric Mintz <16582299+emintz@users.noreply.github.com>
2023-01-01 12:24:29 -0500

    Remove supernumerary pin declarations

--- TestLedMatrix/PwmPinsAndChannels.h
+++ TestLedMatrix/PwmPinsAndChannels.h
@@ -12,30 +12,6 @@
 #ifndef PWMPINSANDCHANNELS_H_
 #define PWMPINSANDCHANNELS_H_
 
-// PWM settings
-#define PWM_FREQUENCY 512
-#define PWM_RESOLUTION 10  // 0 .. 1023
-
-// Acceleration pins:
-#define TILT_BLUE_PIN  16
-#define TILT_GREEN_PIN  4
-#define TILT_RED_PIN    0
-
-// Acceleration PWM channels:
-#define TILT_BLUE_CHANNEL 0
-#define TILT_GREEN_CHANNEL 1
-#define TILT_RED_CHANNEL 2
-
-// Tilt pins:
-#define SHOCK_BLUE_PIN  19
-#define SHOCK_GREEN_PIN 18
-#define SHOCK_RED_PIN   17
-
-// Tilt PWM Channels
-#define SHOCK_BLUE_CHANNEL  3
-#define SHOCK_GREEN_CHANNEL 4
-#define SHOCK_RED_CHANNEL   5
-
 // LED string (or array) pins
 #define LED_ARRAY_PIN 15
 
