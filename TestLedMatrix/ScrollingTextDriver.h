36487caf3df49b0d16558bce710e838b130162ae
Richard Ho <80137887+RQHO@users.noreply.github.com>
2023-03-09 18:12:45 -0500

    Added Scrolling Text Driver

---
+++ TestLedMatrix/ScrollingTextDriver.h
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include "DisplayDriver.h"
+#include "TypeFace.h"
+
+class ScrollingTextDriver : public DisplayDriver {
+public:
+    ScrollingTextDriver(const TypeFace& type_face);
+    virtual ~ScrollingTextDriver();
+
+    virtual BaseType_t begin(
+        const DisplayMessage& displayMessage,
+        Marquee *marquee,
+        QueueHandle_t h_queue);
+
+    virtual BaseType_t refresh(
+        const DisplayMessage& displayMessage,
+        Marquee *marquee,
+        QueueHandle_t h_queue);
+
+private:
+    BaseType_t write_text(
+        const DisplayMessage& displayMessage,
+        Marquee *marquee,
+        QueueHandle_t h_queue);
+
+    const TypeFace& type_face;
+};
