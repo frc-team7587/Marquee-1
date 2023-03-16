8ccbdae929902d189001468882539598597c8207
Eric Mintz <16582299+emintz@users.noreply.github.com>
2023-03-16 11:46:20 -0400

    Convert spaces to tabs and reformat at 2 spaces per indentiation

--- TestLedMatrix/ScrollingTextDriver.cpp
+++ TestLedMatrix/ScrollingTextDriver.cpp
@@ -11,51 +11,51 @@ ScrollingTextDriver::~ScrollingTextDriver() {
 }
 
 BaseType_t ScrollingTextDriver::begin(
-		const DisplayMessage& displayMessage,
-		Marquee *marquee,
-		QueueHandle_t h_queue) {
+    const DisplayMessage& displayMessage,
+    Marquee *marquee,
+    QueueHandle_t h_queue) {
     column_shift = 0;
-	return write_text(displayMessage, marquee, h_queue);
+  return write_text(displayMessage, marquee, h_queue);
 }
 
 BaseType_t ScrollingTextDriver::refresh(
-        const DisplayMessage& displayMessage,
-        Marquee *marquee,
-        QueueHandle_t h_queue) {
-    return write_text(displayMessage, marquee, h_queue);
+    const DisplayMessage& displayMessage,
+    Marquee *marquee,
+    QueueHandle_t h_queue) {
+  return write_text(displayMessage, marquee, h_queue);
 }
 
 BaseType_t ScrollingTextDriver::write_text(
-        const DisplayMessage& displayMessage,
-        Marquee *marquee,
-        QueueHandle_t h_queue) {
-    size_t led_count = marquee->led_count();
-	CRGB background = displayMessage.background;
-	CRGB foreground = displayMessage.foreground;
+      const DisplayMessage& displayMessage,
+      Marquee *marquee,
+      QueueHandle_t h_queue) {
+  size_t led_count = marquee->led_count();
+  CRGB background = displayMessage.background;
+  CRGB foreground = displayMessage.foreground;
 
-	marquee->flood(&background);
+  marquee->flood(&background);
 
-	const unsigned char *p_text = displayMessage.p_text;
-	size_t text_length = displayMessage.text_length;
-	int columns_in_string = type_face.char_width() * text_length;
-	int columns_in_marquee = marquee->columns();
-	int rows = type_face.char_height();
+  const unsigned char *p_text = displayMessage.p_text;
+  size_t text_length = displayMessage.text_length;
+  int columns_in_string = type_face.char_width() * text_length;
+  int columns_in_marquee = marquee->columns();
+  int rows = type_face.char_height();
 
   for (int column = 0; column < columns_in_marquee; column++) {
-      // Stops it from displaying columns past the string
-      if (column + column_shift <= columns_in_string) {
-          continue;
+    // Stops it from displaying columns past the string
+    if (column + column_shift <= columns_in_string) {
+      continue;
+    }
+    for (int row = 0; row < rows; row++) {
+      if (type_face.bit_at(p_text, text_length, row, column + column_shift)) {
+        marquee->set_pixel(row, column, &foreground);
       }
-      for (int row = 0; row < rows; row++) {
-          if (type_face.bit_at(p_text, text_length, row, column + column_shift)) {
-              marquee->set_pixel(row, column, &foreground);
-          }
-      }
+    }
   }
 
   // Increment the column shift for next frame
   column_shift++;
 
-	marquee->show();
+  marquee->show();
     return xQueuePeek(h_queue, &display_message, pdMS_TO_TICKS(20));
 }
--- TestLedMatrix/ScrollingTextDriver.h
+++ TestLedMatrix/ScrollingTextDriver.h
@@ -5,25 +5,25 @@
 
 class ScrollingTextDriver : public DisplayDriver {
 public:
-    ScrollingTextDriver(const TypeFace& type_face);
-    virtual ~ScrollingTextDriver();
+  ScrollingTextDriver(const TypeFace& type_face);
+  virtual ~ScrollingTextDriver();
 
-    virtual BaseType_t begin(
-        const DisplayMessage& displayMessage,
-        Marquee *marquee,
-        QueueHandle_t h_queue);
+  virtual BaseType_t begin(
+      const DisplayMessage& displayMessage,
+      Marquee *marquee,
+      QueueHandle_t h_queue);
 
-    virtual BaseType_t refresh(
-        const DisplayMessage& displayMessage,
-        Marquee *marquee,
-        QueueHandle_t h_queue);
+  virtual BaseType_t refresh(
+      const DisplayMessage& displayMessage,
+      Marquee *marquee,
+      QueueHandle_t h_queue);
 
 private:
-    BaseType_t write_text(
-        const DisplayMessage& displayMessage,
-        Marquee *marquee,
-        QueueHandle_t h_queue);
+  BaseType_t write_text(
+      const DisplayMessage& displayMessage,
+      Marquee *marquee,
+      QueueHandle_t h_queue);
 
-    const TypeFace& type_face;
-    int16_t column_shift;
+  const TypeFace& type_face;
+  int16_t column_shift;
 };
